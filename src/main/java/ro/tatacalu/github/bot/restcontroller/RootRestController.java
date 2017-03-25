8d2798ab591226272600e3e0ba621232d7bc0dab
Matei Dragu <tatacalu@users.noreply.github.com>
2017-03-25 17:25:07 +0000

    Log statements  for RootRestController#index

--- src/main/java/ro/tatacalu/github/bot/restcontroller/RootRestController.java
+++ src/main/java/ro/tatacalu/github/bot/restcontroller/RootRestController.java
@@ -1,5 +1,7 @@
 package ro.tatacalu.github.bot.restcontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +14,15 @@ import java.time.ZonedDateTime;
 @RestController
 public class RootRestController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RootRestController.class);
+
     private static final String GREETING_STRING_FORMAT = "This is matei-bot for Github, current UTC time: %s";
 
     @RequestMapping("/")
     public String index() {
+
+        LOGGER.info("INFO  level - GET / command received");
+        LOGGER.info("DEBUG level - GET / command received");
 
         ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
 

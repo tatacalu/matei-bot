323b16b25587403163f41e3506c526dbd225e216
Matei Dragu <tatacalu@users.noreply.github.com>
2017-03-25 16:17:42 +0000

    Project skeleton - Gragle 3.4.1 + Spring Boot 1.5.2

---
+++ src/main/java/ro/tatacalu/github/bot/MateibotApplication.java
@@ -0,0 +1,12 @@
+package ro.tatacalu.github.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateibotApplication.class, args);
+	}
+}
